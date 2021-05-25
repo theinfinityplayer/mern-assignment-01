6e1284763e9320c9d6f06d18e77d2e1e34f54971
Akki <42275293+theinfinityplayer@users.noreply.github.com>
2021-05-25 16:52:23 +0530

    Update Assign-2.js

--- Assign-2.js
+++ Assign-2.js
@@ -1,3 +1,12 @@
+// pseudocode
+
+//amount= some value
+//rate = some value
+//time =  some value
+//simpleinterest = (amount*rate*time)/100
+//print simple interest =
+
+//Write the pseudocode & a program to calculate the simple interestbased on the inputs(amount, rate, time) provided by the user
 let amt=10000;
 let rate=7;
 let time=3;
