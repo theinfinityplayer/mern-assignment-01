337389d010ba1b2592a168733be6312108ea2f65
Akki <42275293+theinfinityplayer@users.noreply.github.com>
2021-05-25 16:58:04 +0530

    Update Assign-5.js

--- Assign-5.js
+++ Assign-5.js
@@ -1,4 +1,9 @@
-let a=3458967
-let b=0
-
-while()
+//program to count no of digits in a number
+let a=3458967;
+let b=0;
+while(a>0)
+{
+    a=Math.floor(a/10)
+    digits+=1;
+}
+process.stdout.write(digits+"\n");
